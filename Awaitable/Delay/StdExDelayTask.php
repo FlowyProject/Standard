c9dd39f4cda51fc9f67141c05d3ee971c131f9e0
Nerahikada <hkshirorom@yahoo.co.jp>
2018-07-04 01:11:03 +0900

    API 4.0.0 のスケジューラーへアップデート (動作未確認)

--- Awaitable/Delay/StdExDelayTask.php
+++ Awaitable/Delay/StdExDelayTask.php
@@ -1,12 +1,23 @@
 <?php
 namespace StandardExtensions\Awaitable\Delay;
 
-use pocketmine\scheduler\PluginTask;
+use pocketmine\plugin\Plugin;
+use pocketmine\scheduler\Task;
 
 if(!class_exists('StandardExtensions\Awaitable\Delay\StdExDelayTask')) {
 
-    class StdExDelayTask extends PluginTask
+    class StdExDelayTask extends Task
     {
+    	protected $owner;
+
+    	public function __construct(Plugin $owner){
+			$this->owner = $owner;
+		}
+
+		final public function getOwner() : Plugin{
+			return $this->owner;
+		}
+
         public function onRun(int $currentTick)
         {
             $this->owner->handleDelayExMethod($this->getTaskId());
